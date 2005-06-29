806e8ef2eeaff28166478eaab87881bb21c40d94
itamarst <itamarst@a8ab91b2-48ad-11de-82b9-170f4fd74ac7>
2005-06-29 21:41:01 +0000

    reorganize into package

---
+++ coil/__init__.py
@@ -0,0 +1,1 @@
+"""Coil: A Configuration Library."""
---
+++ coil/test/test_struct.py
@@ -0,0 +1,40 @@
+"""Tests for coil.struct."""
+
+from twisted.trial import unittest
+from coil import struct
+
+
+class InitialStructTestCase(unittest.TestCase):
+
+    def setUp(self):
+        server = struct.Struct(None, [('host', "localhost"),
+                                      ('port', 0)])
+        imapClient = struct.Struct(None,
+                                   [('server', server),
+                                    ('username', ""),
+                                    ('password', "")])
+        keychainImap = struct.Struct(None,
+                              [('password', ""),
+                               ('description', "a keychain"),
+                               ('imap', struct.Struct(imapClient, [('password',  struct.Link(struct.CONTAINER, "password"))]))])
+        self.joeKeychain = struct.Struct(keychainImap,
+                                         [('password', "mypassword"),
+                                          ('imap:username', "joe")])
+        self.joenode = struct.StructNode(self.joeKeychain)
+        self.nodesc = struct.Struct(self.joeKeychain, deletedAttrs=("description",))
+        self.nodescNode = struct.StructNode(self.nodesc)
+
+    def testAttribute(self):
+        self.assertEquals(self.joenode.description, "a keychain")
+        self.assertEquals(self.joenode.imap.server.host, "localhost")
+
+    def testLink(self):
+        self.assertEquals(self.joenode.imap.password, "mypassword")
+
+    def testOverride(self):
+        self.assertEquals(self.joenode.imap.username, "joe")
+
+    def testDeletion(self):
+        self.assert_(not hasattr(self.nodescNode, "description"))
+        self.assertEquals(list(self.joeKeychain.attributes()), ["password", "description", "imap"])
+        self.assertEquals(list(self.nodesc.attributes()), ["password", "imap"])
