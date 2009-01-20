edc131ac4d60d91785c50b20763335fb3851c060
marineam <marineam@a8ab91b2-48ad-11de-82b9-170f4fd74ac7>
2009-01-20 04:44:42 +0000

    Lots of fixes, add support for parsing deletes and @extends

--- coil/parser.py
+++ coil/parser.py
@@ -1,41 +1,49 @@
 """Coil Parser"""
 
-from coil import tokenizer, struct
+from coil.tokenizer import Tokenizer, CoilSyntaxError
+from coil.struct import Struct, StructError
 
-class CoilSyntaxError(tokenizer.CoilParseError):
+class CoilDataError(CoilSyntaxError):
     pass
 
 class Parser(object):
     """The standard coil parser"""
 
     def __init__(self, input_, path=None, encoding=None):
-        self._tokenizer = tokenizer.Tokenizer(input_, path, encoding)
+        self._tokenizer = Tokenizer(input_, path, encoding)
 
         # Create the root Struct and parse!
-        self._root = struct.Struct()
+        self._root = Struct()
         self._parse_struct_attributes(self._root)
 
     def root(self):
         """Get the root Struct"""
         return self._root
 
-    def _expect(self, token, *types):
+    def _expect(self, token, types):
         if token.type is None:
             raise CoilSyntaxError(token, "Unexpected end of input, "
                     "looking for: %s" % " ".join(types))
 
         if token.type not in types:
-            raise CoilSyntaxError(token, "Unexpected %s: %s, looking for %s" %
-                    (token.type, repr(token.value), " ".join(types)))
+            if token.type == token.value:
+                unexpected = repr(token.type)
+            else:
+                unexpected = "%s: %s" % (token.type, repr(token.value))
+
+            raise CoilSyntaxError(token, "Unexpected %s, looking for %s" %
+                    (unexpected, " ".join(types)))
 
     def _next(self, *types):
         token = self._tokenizer.next()
-        self._expect(token, *types)
+        if types:
+            self._expect(token, types)
         return token
 
     def _peek(self, *types):
         token = self._tokenizer.peek()
-        self._expect(token, *types)
+        if types:
+            self._expect(token, types)
         return token
 
     def _parse_struct(self, container=None, name=None):
@@ -43,7 +51,7 @@ class Parser(object):
 
         self._next('{')
 
-        new = struct.Struct(container=container, name=name)
+        new = Struct(container=container, name=name)
         self._parse_struct_attributes(new)
 
         self._next('}')
@@ -53,24 +61,69 @@ class Parser(object):
     def _parse_struct_attributes(self, new_struct):
         """attribute..."""
 
-        while self._tokenizer.peek().type not in (None, '}'):
-            self._parse_attribute(new_struct)
+        # Check for attempts to set and/or delete the same thing twice
+        added = set()
+        deleted = set()
 
-    def _parse_attribute(self, container):
+        while self._peek().type not in (None, '}'):
+            self._parse_attribute(new_struct, added, deleted)
+
+    def _parse_attribute(self, container, added, deleted):
         """name: value"""
 
-        name = self._parse_name()
-        self._next(':')
-        value = self._parse_value(container, name)
-        container.set(name, value)
+        token = self._peek('@', '~', 'ATOM')
+
+        if token.type == '@':
+            self._next()
+            name = self._next('ATOM').value
+            self._next(':')
+
+            special = getattr(self, "_special_%s" % name, None)
+            if special is None:
+                raise CoilSyntaxError(token,
+                        "Unknown special attribute: @%s" % name)
+            else:
+                special(container, added, deleted)
+
+        elif token.type == '~':
+            self._next()
+            name = self._parse_name()
+
+            if name in added:
+                raise CoilDataError(token, "Attribute added and "
+                        "deleted in the same structure: %s" % name)
+            elif name in deleted:
+                raise CoilDataError(token, "Attribute deleted twice "
+                        "in the same structure: %s" % name)
+            try:
+                container.delete(name)
+            except StructError, ex:
+                raise CoilDataError(token, ex.message)
+
+        else:
+            name = self._parse_name()
+            self._next(':')
+
+            if name in added:
+                raise CoilDataError(token, "Attribute added twice "
+                        "in the same structure: %s" % name)
+            elif name in deleted:
+                raise CoilDataError(token, "Attribute added and "
+                        "deleted in the same structure %s" % name)
+
+            value = self._parse_value(container, name)
+            try:
+                container.set(name, value)
+            except StructError, ex:
+                raise CoilDataError(token, ex.message)
 
     def _parse_name(self):
         """ATOM[.ATOM]*"""
 
         name = self._next('ATOM').value
 
-        while self._tokenizer.peek().type == '.':
-            self._next('.')
+        while self._peek().type == '.':
+            self._next()
             name = "%s.%s" % (name, self._next('ATOM').value)
 
         return name
@@ -85,7 +138,7 @@ class Parser(object):
         elif token.type == '[':
             return self._parse_list()
         elif token.type in ('@', '.', 'ATOM'):
-            return self._parse_and_follow_path()
+            return self._parse_and_follow_path(container)
         elif token.type in ('INTEGER', 'FLOAT', 'STRING'):
             self._next('INTEGER', 'FLOAT', 'STRING')
             return token.value
@@ -96,8 +149,8 @@ class Parser(object):
         self._next('[')
         new = []
 
-        token = self._next(']', 'INTEGER', 'FLOAT', 'STRING')
         while True:
+            token = self._next(']', 'INTEGER', 'FLOAT', 'STRING')
             if token.type == ']':
                 return new
             else:
@@ -117,7 +170,7 @@ class Parser(object):
             # Loop until we get an ATOM
             next = self._next('.', 'ATOM')
             path += next.value
-            while token.type == '.':
+            while next.type == '.':
                 next = self._next('.', 'ATOM')
                 path += next.value
 
@@ -130,11 +183,25 @@ class Parser(object):
                 raise CoilSyntaxError(token, "Unknown @%s, expected @root" %
                         next.value)
 
-        while self._tokenizer.peek().type == '.':
-            self._next('.')
-            path = "%s.%s" % (path, self._next('ATOM'))
+        while self._peek().type == '.':
+            self._next()
+            path = "%s.%s" % (path, self._next('ATOM').value)
 
         try:
             return container.get(path)
-        except struct.KeyMissingError:
-            raise CoilSyntaxError(token, "Path %s does not exist" % repr(path))
+        except StructError, ex:
+            raise CoilDataError(token, ex.message)
+
+    def _special_extends(self, container, added, deleted):
+        token = self._peek()
+        parent = self._parse_and_follow_path(container)
+        if isinstance(parent, Struct):
+            for key, val in parent.iteritems():
+                # Don't blindly set if if missing, it may have been
+                # deleted (@extends can appear anywhere in the Struct)
+                if key not in added and key not in deleted:
+                    if isinstance(val, Struct):
+                        val = val.copy()
+                    container[key] = val
+        else:
+            raise CoilSyntaxError(token, "@extends doesn't refer to a struct")
--- coil/test/test_parser.py
+++ coil/test/test_parser.py
@@ -28,3 +28,56 @@ class ParserTestCase(unittest.TestCase):
         root = parser.Parser(["foo: { bar: 'baz' }"]).root()
         self.assert_(isinstance(root['foo'], struct.Struct))
         self.assertEquals(root['foo']['bar'], "baz")
+
+    def testExtends(self):
+        root = parser.Parser(["a: {x: 'x'} b: { @extends: ..a }"]).root()
+        self.assertEquals(root['b']['x'], "x")
+
+class FullTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.text = """
+            A: {
+                a: "a"
+                b: "b"
+                c: "c"
+            }
+            B: {
+                @extends: ..A
+                e: [ "one" 2 "omg three" ]
+                ~c
+            }
+            C: {
+                a: ..A.a
+                b: @root.B.b
+            }
+            D: {
+                @extends: @root.B
+            }
+            """
+        self.tree = parser.Parser(self.text.splitlines()).root()
+
+    def testBasic(self):
+        self.assertEquals(self.tree['A']['a'], "a")
+        self.assertEquals(self.tree['A']['b'], "b")
+        self.assertEquals(self.tree['A']['c'], "c")
+        self.assertEquals(len(self.tree['A']), 3)
+
+    def testExtendsAndDelete(self):
+        self.assertEquals(self.tree['B']['a'], "a")
+        self.assertEquals(self.tree['B']['b'], "b")
+        self.assertRaises(KeyError, lambda: self.tree['B']['c'])
+        self.assertEquals(self.tree['B']['e'], [ "one", 2, "omg three" ])
+        self.assertEquals(len(self.tree['B']), 3)
+
+    def testRefrences(self):
+        self.assertEquals(self.tree['C']['a'], "a")
+        self.assertEquals(self.tree['C']['b'], "b")
+        self.assertEquals(len(self.tree['C']), 2)
+
+    def testExtends(self):
+        self.assertEquals(self.tree['D']['a'], "a")
+        self.assertEquals(self.tree['D']['b'], "b")
+        self.assertRaises(KeyError, lambda: self.tree['D']['c'])
+        self.assertEquals(self.tree['D']['e'], [ "one", 2, "omg three" ])
+        self.assertEquals(len(self.tree['D']), 3)
