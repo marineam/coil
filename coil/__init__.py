a17c2ce30f30d0441b1475457b0bc9d04da9f143
marineam <marineam@a8ab91b2-48ad-11de-82b9-170f4fd74ac7>
2009-01-20 17:26:54 +0000

    Add helpers for parsing files and strings

--- coil/__init__.py
+++ coil/__init__.py
@@ -1,3 +1,20 @@
 """Coil: A Configuration Library."""
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
+
+from coil.parser import Parser
+
+def parse_file(file_name):
+    """Open and parse a coil file.
+
+    Returns the root Struct.
+    """
+    coil = open(file_name)
+    return Parser(coil, file_name).root()
+
+def parse(string):
+    """Parse a coil string.
+
+    Returns the root Struct.
+    """
+    return Parser(string.splitlines()).root()
