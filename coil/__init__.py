4af1ebee292d4435ae243bf8de774a071dcf026a
marineam <marineam@a8ab91b2-48ad-11de-82b9-170f4fd74ac7>
2009-05-28 18:33:58 +0000

    Bump to 0.3.6

--- coil/__init__.py
+++ coil/__init__.py
@@ -4,7 +4,7 @@
 
 """Coil: A Configuration Library."""
 
-__version_info__ = (0,3,5)
+__version_info__ = (0,3,6)
 __version__ = ".".join([str(x) for x in __version_info__])
 __all__ = ['struct', 'parser', 'tokenizer', 'errors']
 
